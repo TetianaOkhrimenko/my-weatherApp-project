e447b9ef6f08ecfcd8f0c412eef274cf543a3cc1
TetianaOkhrimenko <TANOH@ukr.net>
2022-10-13 10:56:42 +0100

    Changed description in todayWeather

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function showCity(event) {
 function showTemperature(response) {
   city.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
   temperature.innerHTML = `${Math.round(response.data.main.temp)} °C`;
-  todayWeather.innerHTML = `${response.data.weather[0].main}`;
+  todayWeather.innerHTML = `${response.data.weather[0].description}`;
   humidity.innerHTML = `${response.data.main.humidity}`;
   wind.innerHTML = `${Math.round(response.data.wind.speed)}`;
   document.querySelector(".enter-city-input").value = "";
